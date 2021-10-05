a7c023bcc17e3e8a02b8d55944baee51b8412397
Elisio Quintino <elife599@student.liu.se>
2021-10-05 05:04:10 +0200

    tests added and for-loop in brute-force removed

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -8,15 +8,11 @@ brute_force_knapsack = function(x, W){
   n = nrow(x)
   packed_combinations = 1:(2**n-1)
   brute_force_df = data.frame(row.names = packed_combinations)
-  brute_force_df["weight"] = 0
-  brute_force_df["value"] = 0
-  for (el in row.names(brute_force_df)){
-    choices = which(intToBits(el) == 1)
-    total_choice_weigth = sum(x[["w"]][choices])
-    total_choice_value = sum(x[["v"]][choices])
-    brute_force_df[el,]["weight"] = total_choice_weigth
-    brute_force_df[el,]["value"] = ifelse(total_choice_weigth <= W, total_choice_value, 0)
-  }
+  brute_force_df[["choices"]] = lapply(row.names(brute_force_df), function(inp){which(intToBits(inp) == 1)})
+  brute_force_df[["total_weigth"]] = lapply(brute_force_df[["choices"]], function(inp) {sum(x[["w"]][inp])})
+  brute_force_df[["total_value"]] = lapply(brute_force_df[["choices"]], function(inp) {sum(x[["v"]][inp])})
+  brute_force_df[["is_feasible"]] = lapply(brute_force_df[["total_weigth"]], function(inp){ifelse(inp <= W, 1, 0)})
+  brute_force_df[["value"]] = lapply(row.names(brute_force_df), function(inp){brute_force_df[[inp,"total_value"]] * brute_force_df[[inp,"is_feasible"]]})
   solution = which.max(brute_force_df[["value"]])
   return(list(value = brute_force_df[["value"]][[solution]],
               elements = which(intToBits(solution) == 1)))
