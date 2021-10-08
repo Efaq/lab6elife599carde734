6677d8622d4ba680d06a73617d75f478007dd6e8
Elisio Quintino <elife599@student.liu.se>
2021-10-08 16:41:28 +0200

    Adding comments

--- R/lab6elife599carde734-package.R
+++ R/lab6elife599carde734-package.R
@@ -1,5 +1,8 @@
 #' @details
 #' This package is the solution for Lab 6 in Advanced R.
-#' TODO add description here
+#' The package implements three solvers for the Knapsack problem (see \url{https://en.wikipedia.org/wiki/Knapsack_problem}),
+#' namely the Brute Force Search, the Dynamic Programming Solver and the Greedy Heuristic.
+#' The Brute Force Search offers the possibility of using parallel computation.
+#' Through the package's vignette you can learn about the differences in speed for these algorithms.
 #' @keywords internal
 "_PACKAGE"
